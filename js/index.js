65252aac26b0b91b6e7b0cc61225581821f68d82
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2024-11-25 09:56:09 +0100

    validate form error

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ const dropdownIcon = document.querySelector('.header__navbar__element--second');
 const dropdownList = document.querySelector('.header__navbar__list');
 const hidePage = document.documentElement.scrollHeight;
 const buttonToTop = document.getElementById('goToTop');
+const buttonForm = document.getElementById('formButton');
+
+//EVENTOS
 
 //Evento despliegue de la lista en version movil 
 dropdownIcon.addEventListener('click', () => {
@@ -38,3 +41,64 @@ buttonToTop.addEventListener('click', function () {
     }, 200);
 
 });
+
+//comprobacion previa envio form 
+
+buttonForm.addEventListener('click', function () {
+    const nameInput = document.querySelector(".form__inputLine__inputUserName");
+    const emailInput = document.querySelector(".form__inputLine__inputUserEmail");
+    const consentCheckbox = document.querySelector(".form__consent__checkbox");
+    const isNameValid = validateName(nameInput);
+    const isEmailValid = validateEmail(emailInput);
+    const isConsentGiven = validateConsent(consentCheckbox);
+    const nameError = document.querySelector(".form__inputLine__error--name");
+    const emailError = document.querySelector(".form__inputLine__error--email");
+    const checkBoxForm = document.querySelector(".form__consent__checkbox");
+    if (isNameValid && isEmailValid && isConsentGiven) {
+
+        TODO
+    } else {
+        if (!isNameValid) {
+            nameError.style.visibility = 'visible';
+        }
+        if (!isEmailValid) {
+            emailError.style.visibility = 'visible';
+        }
+        if (!isConsentGiven) {
+            checkBoxForm.style.border = "2px solid red";
+        }
+        if (isNameValid) {
+            nameError.style.visibility = 'hidden';
+        }
+        if (isEmailValid) {
+            emailError.style.visibility = 'hidden';
+        }
+        if (isConsentGiven) {
+            checkBoxForm.style.border = "1px solid gray";
+        }
+
+
+    }
+});
+
+
+
+//FUNCIONES
+// Funciones de validación
+function validateName(input) {
+    const value = input.value.trim();
+    if (value.length >= 2 && value.length <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validateEmail(input) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(input.value.trim());
+}
+
+function validateConsent(checkbox) {
+    return checkbox.checked;
+}
