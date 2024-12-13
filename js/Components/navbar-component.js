a12d00736cd0795faa6d8660ffa5dcdf09581e57
Oscar gracia <113419592+OscarGrC@users.noreply.github.com>
2024-12-13 15:35:01 +0100

    menu click event add

--- js/Components/navbar-component.js
+++ js/Components/navbar-component.js
@@ -5,18 +5,34 @@ class NavBarComponent {
         this.menuIcon = document.getElementById('menuIcon')
         this.dropdownList = document.getElementById('dropdownList')
         this.progressBar = document.getElementById('progressBar')
-
+        this.whyUs = document.getElementById('whyUs')
+        this.benefits = document.getElementById('benefits')
+        this.prices = document.getElementById('prices')
+        this.contact = document.getElementById('contact')
 
         this.initializeEvents()
     }
     initializeEvents() {
         this.menuIcon.addEventListener('click', () => this.dropdownMenu())
+        this.whyUs.addEventListener('click', () => this.dropdownPage("mostPopular"))
+        this.benefits.addEventListener('click', () => this.dropdownPage("secondSection"))
+        this.prices.addEventListener('click', () => this.dropdownPage("pricing"))
+        this.contact.addEventListener('click', () => this.dropdownPage("form"))
         window.addEventListener('scroll', () => this.updateProgressBar())
     }
     // Funcion para que el MenuIcon despliege la lista en movil añadiendo clase 
     dropdownMenu() {
         this.dropdownList.classList.toggle('header__navbar__list--event');
     }
+    dropdownPage(targetId) {
+        const targetSection = document.getElementsByClassName(targetId)[0];
+        console.log(targetId)
+        window.scrollTo({
+            top: targetSection.offsetTop - 1,
+            behavior: 'smooth'
+        });
+    }
+
     // Funcion actualiza el progressBar   
     updateProgressBar() {
         const scrollActualPosition = calculateScrollPosition()
